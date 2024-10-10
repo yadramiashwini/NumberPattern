73de075354401b33c13e74d6682cec16f877341c
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-10 21:12:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NumberPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            int j = 1;
+            do
+            {
+                for( i =1;i<=5;i++)
+                {
+                  for( j = 1; j <= 2; j++)
+                    {
+                        Console.WriteLine(i+" "+j);
+                    }
+                }     
+            }while(i<=5);
+        }
+    }
+}
